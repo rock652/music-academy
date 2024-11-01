418d83635206521d356df3995776a791d6846007
Rahul Yadav <rahulyadav989290@gmail.com>
2024-11-01 09:04:14 +0530

    featured section will be completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import FeaturedCourses from "./components/FeaturedCourses";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[.96] antialiased bg-grid-white/[.02]">
       <HeroSection />
+      <FeaturedCourses />
     </main>
   );
 }
